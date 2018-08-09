3016706b7b87c305a33b1c6875b876819a674927
Kwangmin Yoo <kwangminy27@outlook.com>
2018-08-09 14:18:39 +0900

    K WinAPI Game Framework

---
+++ K WinAPI Game Framework/Inc/K WinAPI Game Framework.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR command_line, int command_show)
+{
+	return 0;
+}
---
+++ K WinAPI Game Framework/Inc/stdafx.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Windows API
+#include <Windows.h>
